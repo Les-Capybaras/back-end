662762c48f95ad5042852d5da93b6a3e725504ce
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2023-02-09 10:46:28 +0100

    init docker stack

---
+++ api/index.js
@@ -0,0 +1,57 @@
+// Vars
+const dotenv = require('dotenv')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { Sequelize } = require('sequelize')
+const amqplib = require('amqplib/callback_api');
+
+// Express App
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+// Select the env file
+
+dotenv.config()
+const PORT = process.env.API_PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`server listen on port ${PORT}`)
+})
+
+// Database connection
+const user = process.env.MARIADB_USER
+const pwd = process.env.MARIADB_PASSWORD
+const db = process.env.MARIADB_DATABASE
+
+const sequelize = new Sequelize(db, user, pwd, {
+  host: 'database', // Docker Service Name
+  dialect: 'mariadb'
+});
+
+async function testAuthenticate () {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testAuthenticate()
+
+app.use('/', (req, res)=>{
+  res.status(200);
+  res.send("Hello World");
+});
+
+const queue = 'messages';
+
+amqplib.connect('amqp://host.docker.internal', (err, conn) => {
+  if (err) throw err;
+
+  if (conn) console.log('Succes connection to Amqp');
+
+});
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Template Docker Express Mq
+
+## What's in it for me ?
+
+- MariaDB Database
+- A PhpMyAdmin instance to manage the DB
+- An empty Express Api
+- Nginx based Web Server
+- A RabbitMq instance
+
+# Installation guide
+
+## What you need
+
+| Runtime / Software |
+| ------------------ |
+| Docker             |
+| Docker Compose     |
+
+## Start the containers with Docker
+
+```sh
+docker-compose up --build
+```
