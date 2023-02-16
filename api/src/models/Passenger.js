912c0714d2f236b72e1a0c56f2f07365f59313ae
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2023-02-16 14:22:25 +0100

    add relation passenger to users / trip

---
+++ api/src/models/Passenger.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+const Trip = require('./Trip');
+const User = require('./User');
+const sequelize = require('../database')()
+
+const Passenger = sequelize.define('Passenger', {
+    state: {
+        type: DataTypes.ENUM,
+        values: ['En attente', 'Accepté'],
+        allowNull: false,
+        defaultValue: 'En attente'
+    },
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    tripId: {
+      type: DataTypes.INTEGER
+    }
+}, { timestamps: false });
+
+User.hasMany(Passenger, {
+    foreignKey: "userId",
+});
+Passenger.belongsTo(User, {
+    foreignKey: "userId",
+});
+Trip.hasMany(Passenger, {
+    foreignKey: "tripId",
+});
+Passenger.belongsTo(Trip, {
+    foreignKey: "tripId",
+});
+
+Passenger.sync({ force: true })
+.then(() => {
+    console.log("Synced Passenger.");
+}) 
+.catch((err) => {
+    console.log("Failed to sync Passenger: " + err.message);
+});
+
+module.exports = Passenger
+
+
+
+
+
